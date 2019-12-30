733f0334482a0fab0d8541e2a4df272d2a9f13cc
Chris <chris_demetrio@hotmail.com>
2019-12-29 22:43:51 -0300

    Login finalizado

---
+++ app/src/main/java/com/example/chatfirebase/ContantsActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_contants);
+    }
+}
